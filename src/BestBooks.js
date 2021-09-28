02b7d09c7a9624f0c1a2709dead4420bdabb1374
ammarzeyad <ammarramadan16@yahoo.com>
2021-09-28 23:03:45 +0300

    have error

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -5,8 +5,7 @@ import "./BestBooks.css";
 import Card from "react-bootstrap/Card";
 import { CardGroup } from "react-bootstrap";
 import axios from "axios";
-
-
+import Forms from "./components/Forms";
 
 class MyFavoriteBooks extends React.Component {
   constructor(props) {
@@ -24,11 +23,45 @@ class MyFavoriteBooks extends React.Component {
     });
     console.log(this.state.books);
   };
+  handleForm = async (event) => {
+    event.preventDefault();
+
+    let BookInfo = {
+      title: event.target.title.value,
+      status: event.target.status.value,
+      Description: event.target.Description.value,
+    }
+    console.log(BookInfo);
+  };
+  handleClose = () => {
+    this.setState({ showModel: false });
+  };
+
+  handleShow = () => {
+    this.setState({ showModel: true });
+  };
+
+  addBook = (info) => {
+    this.setState({ books: info })
+  };
+
+  deleteBook = async (bookID) => {
+    let delURL = await axios.delete(`${process.env.REACT_APP_SERVER}/deleteBook?bookID=${bookID}`);
+    this.setState({ books: delURL.data })
+  };
+
+  // function addBook(event) {
+  //   event.preventDefult()
+
+
+  // }
   render() {
     return (
       <>
-        <h1>My Favorite Books</h1>
+         <h1>My Favorite Books</h1>
         <p>This is a collection of my favorite books</p>
+        <button onClick={this.handleShow}>AddBook</button>
+        {this.state.showModel && <Forms bookF={this.handleForm} showModel={this.state.showModel} close={this.handleClose} addBook={this.addBook.bind(this)}/>}
         {this.state.books && (
           <CardGroup>
             {this.state.books.map((item) => {
---
+++ src/components/Forms.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Form, Button, Modal } from 'react-bootstrap'
+import { withAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+
+class Forms extends React.Component {
+  submitBookInfo = async (e) => {
+    e.preventDefault();
+    const { user } = this.props.auth0;
+    let book = {
+      title: e.target.title.value,
+      description: e.target.description.value,
+      status: e.target.status.value,
+      email: user.email,
+    };
+    let booksData = await axios.post(
+      `${process.env.REACT_APP_SERVER}/books`,
+      book
+    );
+    this.props.addBook(booksData.data);
+    this.props.close();
+  };
+  render() {
+    return (
+      <div>
+
+        <Modal show={this.props.showModel} onHide={this.props.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add Books</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.submitBookInfo.bind(this)}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Book Title</Form.Label>
+                <Form.Control type="text" placeholder="name" name="title" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Book Description</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="description"
+                  name="description"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Book Status</Form.Label>
+                <Form.Control type="text" placeholder="status" name="status" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.props.close}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+
+export default withAuth0(Forms);
